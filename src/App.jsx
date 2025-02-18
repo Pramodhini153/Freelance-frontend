47fd4aac9503a6f6a63c232273c6a1d2d5745824
Pramodhini153 <kotlapramodhini@gmail.com>
2025-02-18 23:53:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Welcome</h1>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
